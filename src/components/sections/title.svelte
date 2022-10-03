0ae778f113e7df0268a71ac1c9114bcebd50342e
Taavi Rübenhagen <112179792+taavirubenhagen@users.noreply.github.com>
2022-10-03 23:06:51 +0200

    Untitled

--- src/components/sections/title.svelte
+++ src/components/sections/title.svelte
@@ -20,7 +20,7 @@
 
 
 
-<div class="relative">
+<div class="h-screen relative">
   <div class="size_screen flex_col_center">
     <Saos once={true} animation={hasAnimation ? g.defaultScrollBottom : ""}>
       <div class="flex_col_center">

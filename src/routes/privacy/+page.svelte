de153bbb4d2599b50a3e518b9da5d609d771c74c
Taavi Rübenhagen <112179792+taavirubenhagen@users.noreply.github.com>
2022-10-28 21:23:27 +0200

    Untitled

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,21 @@
+<div>
+  <h2>Privacy Policy</h2>
+  <div className="legal_page_content">
+    <h5>This privacy policy applies to the following apps and websites:</h5><br/>
+    - "The Presentation Master." in the Play Store<br/>
+    - "Presentation Master - Remote" in the App Store, which will also be renamed to "The Presentation Master." soon<br/>
+    - The websites under "tavy-ai.web.app", "ffluentapps.web.app" and "presentation-master.web.app"<br/>
+    <br/><br/><br/>
+    <h5>We currently do not collect any data from you. Any sensitive data used is stored locally on your devices.</h5>
+    <br/><br/><br/>
+    <h5>Privacy policies of other websites</h5><br/>
+      The apps/websites contain links to other apps or websites. Our privacy policy applies only to the apps/websites mentioned above, so if you click on a link to another website or app, you should read their privacy policy.<br/>
+    <br/><br/><br/>
+    <h5>Changes to our privacy policy</h5><br/>
+      We keep our privacy policy under regular review and places any updates on this web page. This privacy policy was last updated on 30 July 2022.<br/>
+    <br/><br/><br/>
+    <h5>How to contact us</h5><br/>
+      If you have any questions about this privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, please do not hesitate to contact us.<br/>
+      Email us at: tavyai.apps@gmail.com.
+  </div>
+</div>
